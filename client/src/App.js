9f1b324d76daf88e59f295fcca174b7004a93c38
Wyatt Core <99035317+WyattCore@users.noreply.github.com>
2025-03-02 22:05:31 -0600

    Update App.js
    deployment fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import React, {useEffect, useState } from 'react';
 import Header from './header.jsx';
 import Home from './Home.jsx';
 import Plans from './Plans.jsx';
 import About from './About.jsx';
-import {BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import QuestionForm from './QuestionForm.jsx';
-import Plan_details from './Plan_details.jsx';
+import Plan_Details from './Plan_details.jsx';
 
 
 function App(){
@@ -20,7 +19,7 @@ function App(){
             <Route path='/questionform' element={<QuestionForm />} />
             <Route path="/plans" element={<Plans />} /> 
             <Route path="/about" element={<About />} />
-            <Route path="/plan_details/:id" element={<Plan_details />} />
+            <Route path="/plan_details/:id" element={<Plan_Details />} />
         </Routes>
     </Router>
     </>
